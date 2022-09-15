aa5e66e2dbd65371b7c700c984b6906f15a7fa17
satoyu19 <21cm0119@jec.ac.jp>
2022-09-15 14:19:05 +0900

    add Module

---
+++ app/src/main/java/com/example/foody/RemoteDataSource.kt
@@ -0,0 +1,5 @@
+package com.example.foody
+
+class RemoteDataSource() {
+
+}
---
+++ app/src/main/java/com/example/foody/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.example.foody.di
+
+import com.example.foody.models.Constants.Companion.BASE_URL
+import com.example.foody.models.FoodRecipesApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient{
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)      //新規接続時のデフォルトの読み込みタイムアウト
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory{
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient, gsonConverterFactory: GsonConverterFactory): Retrofit{  //gsonConverterFactoryはprovideConverterFactory()から依存性注入
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)       //リクエストに使用されるHTTPクライアント
+            .addConverterFactory(gsonConverterFactory)      //オブジェクトのシリアライズ、デシリアライズのためのコンバータファクトリ
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): FoodRecipesApi{  //retrofitはprovideRetrofitInstanceから依存性注入
+        return retrofit.create(FoodRecipesApi::class.java)
+    }
+
+}
